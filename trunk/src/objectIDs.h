86ef87a6e06c1162f8a1de9caea2a9af081aa5fc
binary1230 <binary1230@gmail.com>
2005-10-17 03:50:40 +0000

    preliminary objectFactory skeleton

---
+++ trunk/src/objectIDs.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_IDS_H
+#define OBJECT_IDS_H
+
+// ID's of objects that you can create from the object factory
+// NOTE: These are just examples for now, we'll replace them with real
+// objects later on.
+
+#define OBJECT_ID_SONIC 	1
+#define OBJECT_ID_TAILS		2
+#define OBJECT_ID_RING		3
+#define OBJECT_ID_SUN			4
+
+#endif // OBJECT_IDS_H
