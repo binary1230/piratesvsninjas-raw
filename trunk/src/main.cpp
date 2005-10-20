7842798c7e5eea86e9d50dc451dd43546ed6644e
binary1230 <binary1230@gmail.com>
2005-10-20 02:50:15 +0000

    game options work

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -3,6 +3,8 @@
 
 #include "gameState.h"
 
+
+
 int main(int argc, char* argv) {
 				// process command line arguments
 				// options[] = process_args(argc, argv);
---
+++ trunk/src/options.h
@@ -0,0 +1,8 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+struct GameOptions {
+	bool fullscreen;
+};
+
+#endif // ARGS_H
