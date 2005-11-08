7e0a5acd6a3fa621bfa62a1e62f5ea8c8a6ab7b0
binary1230 <binary1230@gmail.com>
2005-11-08 08:16:54 +0000

    beginning unit test support

--- branches/new-physics-engine-0.1/autogen.sh
+++ branches/new-physics-engine-0.1/autogen.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+EXTRA_ACLOCAL_PATH="/home/dcerquet/share/aclocal/"
+
 echo "Regenerating autoconf/automake stuff.."
 
+# aclocal stuff with extra paths
 if [ ! -e "aclocal.m4" ]; then 
 	echo "-- aclocal.."
-	aclocal
+	if [ -e "$EXTRA_ACLOCAL_PATH" ]; then
+		echo "-- - Searching extra path: $EXTRA_ACLOCAL_PATH"
+		aclocal -I "$EXTRA_ACLOCAL_PATH"
+	else 
+		echo "-- - Not searching extra path: $EXTRA_ACLOCAL_PATH"
+		aclocal
+	fi
 else 
 	echo "-- skipping aclocal.."
 fi
