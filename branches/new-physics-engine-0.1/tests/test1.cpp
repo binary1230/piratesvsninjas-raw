423b55e7074ff66a141f2112b4f554bf6a8de5f0
binary1230 <binary1230@gmail.com>
2005-11-08 08:17:35 +0000

    beginning unit test support

---
+++ branches/new-physics-engine-0.1/tests/test1.cpp
@@ -0,0 +1,20 @@
+#include "test1.h"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( MoneyTest );
+
+void 
+MoneyTest::setUp()
+{
+}
+
+void 
+MoneyTest::tearDown()
+{
+}
+
+void 
+MoneyTest::testConstructor()
+{
+  CPPUNIT_FAIL( "not implemented" );
+}
