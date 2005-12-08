79eb05fc5a5a36965aa77ec99e3cae92cccdc929
binary1230 <binary1230@gmail.com>
2005-12-08 18:20:04 +0000

    resource loader test

---
+++ branches/new-physics-engine-0.1/src/tests/test_resourceLoader.cpp
@@ -0,0 +1,20 @@
+#include "test_resourceLoader.h"
+
+//! Unit tests for Vector2D class
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( Test_ResourceLoader );
+
+//! Test constructors
+void Test_ResourceLoader::testConstructors() {
+	ResourceLoader rl;
+}
+
+//! INIT: Setup the tests
+void Test_ResourceLoader::setUp() {
+	
+}
+
+//! CLEANUP: Teardown the tests
+void Test_ResourceLoader::tearDown() {
+}
