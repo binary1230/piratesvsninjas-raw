16776ba4142a8b41484adaa2e8f61d84effcf3cf
binary1230 <binary1230@gmail.com>
2006-06-18 02:19:31 +0000

    rectangle class unit tests, updates

--- trunk/src/tests/test_rect.cpp
+++ trunk/src/tests/test_rect.cpp
@@ -11,30 +11,30 @@ void Test_Rect::testConstructors() {
 	CPPUNIT_ASSERT(	r.getx1() == 0.0f && r.gety1() == 0.0f && 
 									r.getx2() == 0.0f && r.gety2() == 0.0f );
 
-	Rect r1(1.1f, 4.2f, 3.3f, 2.4f);
-	CPPUNIT_ASSERT(	r1.getx1() == 1.1f && r1.gety1() == 4.2f && 
-									r1.getx2() == 3.3f && r1.gety2() == 2.4f );
+	Rect r1(1.1f, 2.2f, 3.3f, 4.4f);
+	CPPUNIT_ASSERT(	r1.getx1() == 1.1f && r1.gety1() == 2.2f && 
+									r1.getx2() == 3.3f && r1.gety2() == 4.4f );
 
 	Rect r2 = r1;
 	CPPUNIT_ASSERT(r1 == r2);
 	CPPUNIT_ASSERT(r2 == r1);
-	CPPUNIT_ASSERT(	r2.getx1() == 1.1f && r2.gety1() == 4.2f && 
-									r2.getx2() == 3.3f && r2.gety2() == 2.4f );
+	CPPUNIT_ASSERT(	r2.getx1() == 1.1f && r2.gety1() == 2.2f && 
+									r2.getx2() == 3.3f && r2.gety2() == 4.4f );
 
-	CPPUNIT_ASSERT(	r1.getx1() == 1.1f && r1.gety1() == 4.2f && 
-									r1.getx2() == 3.3f && r1.gety2() == 2.4f );
+	CPPUNIT_ASSERT(	r1.getx1() == 1.1f && r1.gety1() == 2.2f && 
+									r1.getx2() == 3.3f && r1.gety2() == 4.4f );
 
 	// test the swapping via Rect::Fix()
 	float d1 = 0.0f, d2 =  -20.0f, d3 = -1.0f, d4 = 20.0f;
-	Rect r3(d1, d2, d3, d4);
+	Rect r3(d1, d4, d3, d2);
 	Rect r4 = r3;
 	CPPUNIT_ASSERT(r4 == r3);
 	CPPUNIT_ASSERT(r3 == r4);
-	CPPUNIT_ASSERT(	r4.getx1() == d3 && r4.gety1() == d4 && 
-									r4.getx2() == d1 && r4.gety2() == d2 );
+	CPPUNIT_ASSERT(	r4.getx1() == d3 && r4.gety1() == d2 && 
+									r4.getx2() == d1 && r4.gety2() == d4 );
 
-	CPPUNIT_ASSERT(	r3.getx1() == d3 && r3.gety1() == d4 && 
-									r3.getx2() == d1 && r3.gety2() == d2 );
+	CPPUNIT_ASSERT(	r3.getx1() == d3 && r3.gety1() == d2 && 
+									r3.getx2() == d1 && r3.gety2() == d4 );
 
 }
 
@@ -109,50 +109,42 @@ void Test_Rect::testGetSet() {
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 should NOT have swapped", r.getx2(), d2);
 	
 	r.Clear();
-	r.sety1(d4); r.sety2(d3); r.Fix();
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d4);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d3);
+	r.sety1(d3); r.sety2(d4); r.Fix();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d4);
 
-	r.setx1(d1); r.setx2(d2); r.sety1(d4); r.sety2(d3); r.Fix();
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 should NOT have swapped", r.getx1(), d1);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 should NOT have swapped", r.getx2(), d2);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d4);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d3);
-
-	// test where X's should swap
-	r.setx1(d2); r.setx2(d1); r.sety1(d4); r.sety2(d3); r.Fix();
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 SHOULD have swapped", r.getx1(), d1);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 SHOULD have swapped", r.getx2(), d2);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d4);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d3);
-	
-	// test where Y's should swap
 	r.setx1(d1); r.setx2(d2); r.sety1(d3); r.sety2(d4); r.Fix();
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 should NOT have swapped", r.getx1(), d1);
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 should NOT have swapped", r.getx2(), d2);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 SHOULD have swapped", r.gety1(), d4);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 SHOULD have swapped", r.gety2(), d3);
-	
-	// test where both X's and Y's should swap
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d4);
+
+	// test where X's should swap
 	r.setx1(d2); r.setx2(d1); r.sety1(d3); r.sety2(d4); r.Fix();
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 SHOULD have swapped", r.getx1(), d1);
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 SHOULD have swapped", r.getx2(), d2);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 SHOULD have swapped", r.gety1(), d4);
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 SHOULD have swapped", r.gety2(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 should NOT have swapped", r.gety1(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 should NOT have swapped", r.gety2(), d4);
+	
+	// test where Y's should swap
+	r.setx1(d1); r.setx2(d2); r.sety1(d4); r.sety2(d3); r.Fix();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 should NOT have swapped", r.getx1(), d1);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 should NOT have swapped", r.getx2(), d2);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 SHOULD have swapped", r.gety1(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 SHOULD have swapped", r.gety2(), d4);
+	
+	// test where both X's and Y's should swap
+	r.setx1(d2); r.setx2(d1); r.sety1(d4); r.sety2(d3); r.Fix();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("x1 SHOULD have swapped", r.getx1(), d1);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("x2 SHOULD have swapped", r.getx2(), d2);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y1 SHOULD have swapped", r.gety1(), d3);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("y2 SHOULD have swapped", r.gety2(), d4);
 }
 
 //! INIT: Setup the tests
 void Test_Rect::setUp() {
-
-	static float set[] = {	0.0f, -1.0f, 1.0f, 5.0f, -5.0f, 0.00001f, 
-									-0.00001f, 100000.0f, -100000.0f };
-
-	testing_set = (float*)malloc(sizeof(set));
-	memcpy(testing_set, set, sizeof(set));
-	count = sizeof(set) / sizeof(float);
 }
 
 //! CLEANUP: Teardown the tests
 void Test_Rect::tearDown() {
-	free(testing_set);
 }
