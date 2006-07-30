d28f7382f476fa8d7e8c9644b7973c0252770eee
binary1230 <binary1230@gmail.com>
2006-07-30 07:56:57 +0000

    tidying on SF

--- branches/new-physics-engine-0.1/deprecated_branch/src/basename.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASENAME_H
-#define BASENAME_H
-
-//! Returns a pointer to the last component of name
-//! Behavior is undefined if the pathname ends in a directory separator.
-char* basename (const char* name);
-
-#endif // BASENAME_H
--- branches/new-physics-engine-0.1/deprecated_branch/src/gameMode.cpp
+++
@@ -1,4 +0,0 @@
-#include "gameMode.h"
-
-GameMode::GameMode() : next(NULL), parent(NULL) {}
-GameMode::~GameMode() {}
