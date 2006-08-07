db2dfd1df38cc4fd40af297688bbb82b49dfea79
binary1230 <binary1230@gmail.com>
2006-08-07 05:46:41 +0000

    installer added

--- trunk/ninjas-engine-devcpp_private.h
+++ trunk/ninjas-engine-devcpp_private.h
@@ -5,19 +5,19 @@
 #define NINJAS-ENGINE-DEVCPP_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.1.1.1"
+#define VER_STRING	"0.2.8.652"
 #define VER_MAJOR	0
-#define VER_MINOR	1
-#define VER_RELEASE	1
-#define VER_BUILD	1
+#define VER_MINOR	2
+#define VER_RELEASE	8
+#define VER_BUILD	652
 #define COMPANY_NAME	""
-#define FILE_VERSION	""
-#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define FILE_VERSION	"0.2-beta8"
+#define FILE_DESCRIPTION	"Pirates Vs Ninjas"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	""
-#define PRODUCT_VERSION	""
+#define ORIGINAL_FILENAME	"ninjas.exe"
+#define PRODUCT_NAME	"Pirates Vs Ninjas"
+#define PRODUCT_VERSION	"0.2-beta8"
 
 #endif /*NINJAS-ENGINE-DEVCPP_PRIVATE_H*/
