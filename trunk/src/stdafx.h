ccdfd77e7c037db97e803000a352513725c349ed
binary1230 <binary1230@gmail.com>
2006-09-07 03:50:12 +0000

    precompiled header (more)

---
+++ trunk/src/stdafx.h
@@ -0,0 +1,49 @@
+// Precompiled header file.
+
+// ONLY PUT SYSTEM-LEVEL INCLUDES HERE
+// do NOT put anything that is in the game engine in here,
+// with the exception of rarely-modified stuff
+
+#ifndef STDAFX_H
+#define STDAFX_H
+
+// The order of these two is SUPER-IMPORTANT
+#include <allegro.h>
+#ifdef WIN32
+#include <winalleg.h>
+#endif
+
+#ifdef PLATFORM_DARWIN
+#include <CoreServices/CoreServices.h>
+#endif // PLATFORM_DARWIN
+
+// Common to everything
+#include <assert.h>
+#include <malloc.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Unix-y?
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// STL stuff
+#include <algorithm>
+#include <list>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+// Our rarely-modified engine stuff
+#include "StdString.h"
+#include "xmlParser.h"
+#include "alogg.h"
+#include "loadpng.h"
+
+#endif // STDAFX_H
