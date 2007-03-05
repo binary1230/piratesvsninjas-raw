43100f99c522b0e65eec646633c8c0bffbc2e35f
binary1230 <binary1230@gmail.com>
2007-03-05 06:58:03 +0000

    Even more lua support, textboxes working\!

---
+++ trunk/dist/scripts/level2.lua
@@ -0,0 +1,8 @@
+-- Level 2 - Jouten's Lair
+
+function Level_OnLoad() 
+
+	msg = "You found the|'cosmic door'.| |Huzzah.~As a reward, I'll|tell you a little|secret.  If you press|the \"D\" key,|you will drop little|airballs.~Don't ask me why|Jouten can do it.|He's just a|tech guy.~I mean, Toneriko|is understandable,|since she's...| |Oops.|No plot for you yet!~Anyway, press \"C\"|to start.  Try and|find that 'cosmic|door' again.";
+
+	world_textbox(msg, "amaya-icon.png");
+end
