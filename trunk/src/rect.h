db2a5501c93b15ada877743a874aba166c820106
binary1230 <binary1230@gmail.com>
2007-11-18 20:15:44 +0000

    gcc compile fix

--- trunk/src/rect.h
+++ trunk/src/rect.h
@@ -84,7 +84,7 @@ class _Rect {
 		//! Create a rect from 2 vectors
 		// void fromVec(const Vector2D &v1, const Vector2D &v2);
 
-		inline bool _Rect::Overlaps(const _Rect &rect) const {	
+		inline bool Overlaps(const _Rect &rect) const {	
 			return ( y1 <= rect.y2 && y2 >= rect.y1 && x2 >= rect.x1 && x1 <= rect.x2 );
 		}
 		
