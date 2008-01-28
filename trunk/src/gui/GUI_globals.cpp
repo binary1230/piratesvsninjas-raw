f1d06a8c764542e4ac85e59b28c485aa3eb45cf1
binary1230 <binary1230@gmail.com>
2008-01-28 03:13:26 +0000

    Map editor GUI add missing files, take 2

---
+++ trunk/src/gui/GUI_globals.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "precompiled.h"
+#include "GUI_globals.h"
+
+void __cdecl OnLayerSelectionChanged(class Fl_Menu_Button * pkSelection, void *pkUserData)
+{
+	int x = 3;
+}
